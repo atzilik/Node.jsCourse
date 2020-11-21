b5a1358ab4d1ff3cbee20179c484411f0f8dc79e
Alon Tsilik <alont@codevalue.com>
2020-11-21 18:47:54 +0200

    clean code

--- app-template/src/utils/common.ts
+++ app-template/src/utils/common.ts
@@ -8,7 +8,7 @@ export function isValidName(name: string): boolean {
   return name.length > 3;
 }
 
-export const validateNameHandler = (req: Request, res: Response, next: NextFunction): void =>{
+export const validateNameHandler = (req: Request, res: Response, next: NextFunction): void => {
   if (!isValidName(req.body.name)) {
     res.sendStatus(409);
     return;
